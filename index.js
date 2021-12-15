0d3c1d325afff91d5ede82ff5b39787992e3fca5
MMartina020 <marcsikaa15@gmail.com>
2021-12-15 14:01:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const express = require('express');
+const html = require('html');
+
+const port = 4444;
+
+const server = html.createServer((req, res) => {
+    
+});
+
+server.listen(port);
